d4bf209713453586228e06d09b82418d0debc407
ruhulsinbad <ruhulsinbad@gmail.com>
2022-04-30 22:54:46 +0600

    Hero Section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Announcement from "./components/Announcement/Announcement";
+import HeroSection from "./components/layouts/HeroSection";
 import TopMiddle from "./components/layouts/middle/TopMiddle";
 import Sidebar from "./components/layouts/sidebar/Sidebar";
 import Navbar from "./components/navbar/Navbar";
@@ -8,7 +9,8 @@ function App() {
     <div>
       <Announcement />
       <Navbar />
-      <div className=" mx-4 lg:flex lg:justify-between lg:mx-12 xl:mx-48">
+      <HeroSection />
+      <div className=" mt-16 mx-4 lg:flex lg:justify-between lg:mx-12 xl:mx-48">
         <TopMiddle />
         <Sidebar />
       </div>
---
+++ src/components/layouts/HeroSection.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className=" mt-8 h-full grid grid-cols-4 grid-rows-4 lg:mx-48">
+      <img
+        className=" col-span-2 row-span-2"
+        src="https://cdn.vox-cdn.com/thumbor/jQ6gN9iZeDFkTVxBZSZw-e1JHA4=/0x0:4964x3309/750x500/filters:focal(1817x542:2611x1336):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/70810590/1236892070.0.jpg"
+        alt="nope"
+      />
+
+      <div className=" row-span-2">
+        <img
+          src="https://cdn.vox-cdn.com/thumbor/2LEs7QzjNW2rG5iozQtz0hG1Ftc=/0x0:3768x5652/269x239/filters:focal(1462x1977:2064x2579):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/70814089/1393580815.0.jpg"
+          alt="nag"
+        />
+        <div>
+          <p>
+            Daily Schmankerl: Bayern Munich boss Julian Nagelsmann makes his
+            pitch to keep Robert Lewandowski; Shocker — James Rodriguez wants to
+            leave Al Rayyan; Arturo Vidal wants Flamengo move; Paul Pogba set to
+            leave Manchester United?; and MORE!
+          </p>
+        </div>
+      </div>
+
+      <div className=" row-span-2">
+        <img
+          src="https://cdn.vox-cdn.com/thumbor/2LEs7QzjNW2rG5iozQtz0hG1Ftc=/0x0:3768x5652/269x239/filters:focal(1462x1977:2064x2579):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/70814089/1393580815.0.jpg"
+          alt="nag"
+        />
+        <div>
+          <p>
+            Daily Schmankerl: Bayern Munich boss Julian Nagelsmann makes his
+            pitch to keep Robert Lewandowski; Shocker — James Rodriguez wants to
+            leave Al Rayyan; Arturo Vidal wants Flamengo move; Paul Pogba set to
+            leave Manchester United?; and MORE!
+          </p>
+        </div>
+      </div>
+
+      {/* Upper Side  */}
+
+      <div className=" row-span-2">
+        <img
+          src="https://cdn.vox-cdn.com/thumbor/2LEs7QzjNW2rG5iozQtz0hG1Ftc=/0x0:3768x5652/269x239/filters:focal(1462x1977:2064x2579):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/70814089/1393580815.0.jpg"
+          alt="nag"
+        />
+        <div>
+          <p>
+            Daily Schmankerl: Bayern Munich boss Julian Nagelsmann makes his
+            pitch to keep Robert Lewandowski; Shocker — James Rodriguez wants to
+            leave Al Rayyan; Arturo Vidal wants Flamengo move; Paul Pogba set to
+            leave Manchester United?; and MORE!
+          </p>
+        </div>
+      </div>
+
+      <div className=" row-span-2">
+        <img
+          src="https://cdn.vox-cdn.com/thumbor/2LEs7QzjNW2rG5iozQtz0hG1Ftc=/0x0:3768x5652/269x239/filters:focal(1462x1977:2064x2579):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/70814089/1393580815.0.jpg"
+          alt="nag"
+        />
+        <div>
+          <p>
+            Daily Schmankerl: Bayern Munich boss Julian Nagelsmann makes his
+            pitch to keep Robert Lewandowski; Shocker — James Rodriguez wants to
+            leave Al Rayyan; Arturo Vidal wants Flamengo move; Paul Pogba set to
+            leave Manchester United?; and MORE!
+          </p>
+        </div>
+      </div>
+
+      <img
+        className=" col-span-2 row-span-2"
+        src="https://cdn.vox-cdn.com/thumbor/jQ6gN9iZeDFkTVxBZSZw-e1JHA4=/0x0:4964x3309/750x500/filters:focal(1817x542:2611x1336):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/70810590/1236892070.0.jpg"
+        alt="nope"
+      />
+    </div>
+  );
+};
+
+export default HeroSection;
